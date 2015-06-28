ae291132c0682813e33751ec3e83d7eac8c06a34
schuchard <kbs177@gmail.com>
2015-06-27 18:26:06 -0600

    loading easter egg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
-$(document).on('ready', function() {
-  
-});
+(function ($) {
+
+var animate = runAnimation();
+
+    function runAnimation() {
+        return {
+            spin: spin
+        }
+        // Rotate star by 'd' degree's
+        // TODO: add element argument
+        function spin(d) {
+            var elem = $('.landingStar');
+            $({deg: 0}).animate({deg: d}, {
+                duration: 2500,
+                step: function (now) {
+                    elem.css({
+                        transform: 'rotate(' + now + 'deg)'
+                    });
+                }
+            });
+        }
+    }
+
+
+    $(document).on('ready', function() {
+
+      setTimeout(function (argument) {
+          animate.spin(360);
+      },1500);
+
+    });
+
+})(jQuery);
